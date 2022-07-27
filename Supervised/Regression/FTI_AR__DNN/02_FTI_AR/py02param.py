4d08688763fbb79f1b5a1e3d08da7752ac2594e4
yoshisatoh <73818039+yoshisatoh@users.noreply.github.com>
2022-07-27 09:44:01 +0900

    Add files via upload

---
+++ Supervised/Regression/FTI_AR__DNN/02_FTI_AR/py02param.py
@@ -0,0 +1,13 @@
+# moving average days for asset mean returns (mu), standard deviations (sigma), and correlations (rho)
+dma = (250 * 1)
+#dma = 20 * 1
+
+
+# percentile rank: moving average lookback days for FTI
+FTI_pct_rank_dma = 20 * 1
+#FTI_pct_rank_dma = 250 * 3
+
+
+# percentile rank: time period of percentile rank calculation
+#FTI_pct_rank_dtp = 20 * 6
+FTI_pct_rank_dtp = 250 * 1
